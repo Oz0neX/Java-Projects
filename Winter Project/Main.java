13b5069b96755e20fe0620104d0ebc59f8874793
Oz0ne <67258053+Oz0neX@users.noreply.github.com>
2022-05-10 22:37:32 -0700

    Create Main.java

---
+++ Winter Project/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    // Rule the World is a game where your actions have decisions on your future outcome. Choose to be a king or queen and Rule the World!
+    
+    public static void main(String[] args) {
+        new Game(); // Start Game
+        Game.closingMessage();
+    }
+}
